9f486f7af4fe3712ab15cf138797b58d713f2667
apearce <apearce@4525493e-7705-40b1-a816-d608a930855b>
2013-10-02 10:26:05 +0000

    Add methods to stripping and reconstruction efficiency modules which calculate the respective efficiencies using signal yields from the bookkeeping. This is to check that the MCGen ntuples contain all signal candidates.
    git-svn-id: svn+ssh://svn.cern.ch/reps/lhcb/Urania/trunk/Phys/Lc2pXX@163159 4525493e-7705-40b1-a816-d608a930855b

--- python/lc2pxx/efficiencies/reconstruction.py
+++ python/lc2pxx/efficiencies/reconstruction.py
@@ -29,3 +29,60 @@ def efficiency(mode, polarity, year):
 
     return utilities.efficiency_from_yields(reco_num, acc_num)
 
+
+def efficiency_from_bk(mode, polarity, year):
+    """Return the efficiency of the reconstruction.
+
+    This is similar to the `efficiency` method, except rather than using
+    the number of accepted events from the generator-level ntuple, it
+    uses the number from the bookkeeping.
+    This method is here as a placeholder until I fix the MC options, as
+    they are not currently selecting all generated decays.
+    """
+    bk_numbers = {
+        2011: {
+            config.pKpi: {
+                config.magup: 1007497,
+                config.magdown: 1017497
+            },
+            config.pKK: {
+                config.magup: 1002245,
+                config.magdown: 1013743
+            },
+            config.ppipi: {
+                config.magup: 1015499,
+                config.magdown: 1018996
+            }
+        },
+        2012: {
+            config.pKpi: {
+                config.magup: 0,
+                config.magdown: 0
+            },
+            config.pKK: {
+                config.magup: 0,
+                config.magdown: 0
+            },
+            config.ppipi: {
+                config.magup: 0,
+                config.magdown: 0
+            }
+        }
+    }
+
+    for y in bk_numbers:
+        for m in bk_numbers[y]:
+            effs = bk_numbers[y][m]
+            up = effs[config.magup]
+            down = effs[config.magdown]
+            bk_numbers[y][m][config.magboth] = up + down
+
+    acc_num = bk_numbers[year][mode][polarity]
+    truth = "Lambdac_BKGCAT < 20 && Lambdab_BKGCAT < 60"
+    reco_ntuple = ntuples.get_ntuple(
+        mode, polarity, year, mc=True, mc_type=config.mc_cheated
+    )
+    reco_num = reco_ntuple.GetEntries(truth)
+
+    return utilities.efficiency_from_yields(reco_num, acc_num)
+
--- python/lc2pxx/efficiencies/stripping.py
+++ python/lc2pxx/efficiencies/stripping.py
@@ -26,3 +26,60 @@ def efficiency(mode, polarity, year):
 
     return utilities.efficiency_from_yields(num_stripped, num_reco)
 
+
+def efficiency_wrt_acceptance(mode, polarity, year):
+    """Return the stripping efficiency.
+
+    This efficiency represents the ratio
+        No. of stripped candidates / No. of accepted candidates
+    This is then similar to `efficiency`, but with respect to the number
+    of generated signal decays, obtained from the bookkeeping.
+    """
+    bk_numbers = {
+        2011: {
+            config.pKpi: {
+                config.magup: 1007497,
+                config.magdown: 1017497
+            },
+            config.pKK: {
+                config.magup: 1002245,
+                config.magdown: 1013743
+            },
+            config.ppipi: {
+                config.magup: 1015499,
+                config.magdown: 1018996
+            }
+        },
+        2012: {
+            config.pKpi: {
+                config.magup: 0,
+                config.magdown: 0
+            },
+            config.pKK: {
+                config.magup: 0,
+                config.magdown: 0
+            },
+            config.ppipi: {
+                config.magup: 0,
+                config.magdown: 0
+            }
+        }
+    }
+
+    for y in bk_numbers:
+        for m in bk_numbers[y]:
+            effs = bk_numbers[y][m]
+            up = effs[config.magup]
+            down = effs[config.magdown]
+            bk_numbers[y][m][config.magboth] = up + down
+
+    num_accepted = bk_numbers[year][mode][polarity]
+
+    truth_matching = "Lambdab_BKGCAT < 60 && Lambdac_BKGCAT < 20"
+    stripped_ntuple = ntuples.get_ntuple(
+        mode, polarity, year, mc=True, mc_type=config.mc_stripped
+    )
+    num_stripped = stripped_ntuple.GetEntries(truth_matching)
+
+    return utilities.efficiency_from_yields(num_stripped, num_accepted)
+
